7d6435a0047fc38d61d6c28ea0e47f932472365e
James Dale <jmdale@smu.edu>
2018-06-10 22:14:18 -0500

    MD - update main to handle changed inputsystem handling of mouse input.

--- Programs/TankWar3D/Code/Game/Main_Win32.cpp
+++ Programs/TankWar3D/Code/Game/Main_Win32.cpp
@@ -21,86 +21,92 @@ bool AppMessageHandler( unsigned int wmMessageCode, size_t wParam, size_t lParam
 	switch( wmMessageCode )
 	{
 		// App close requested via "X" button, or right-click "Close Window" on task bar, or "Close" from system menu, or Alt-F4
-		case WM_CLOSE:		
-		{
-			g_isQuitting = true;
-			return false; // "Consumes" this message (tells Windows "okay, we handled it")
-		}
+	case WM_CLOSE:		
+	{
+		g_isQuitting = true;
+		return false; // "Consumes" this message (tells Windows "okay, we handled it")
+	}
 
-		// Raw physical keyboard "key-was-just-depressed" event (case-insensitive, not translated)
-		case WM_KEYDOWN:
-		{
-			unsigned char asKey = (unsigned char) wParam;
-			InputSystem::GetInstance()->ProcessKeyDown(asKey);
+	// Raw physical keyboard "key-was-just-depressed" event (case-insensitive, not translated)
+	case WM_KEYDOWN:
+	{
+		unsigned char asKey = (unsigned char) wParam;
+		InputSystem::GetInstance()->ProcessKeyDown(asKey);
 
-			// If ESC, Quit the app
-			/*if(asKey == VK_ESCAPE && !(DevConsole::GetInstance()->IsOpen())) 
-			{
-			g_isQuitting = true;
-			return false;
-			}*/
-
-			break;
-		}
-
-		// Raw physical keyboard "key-was-just-released" event (case-insensitive, not translated)
-		case WM_KEYUP:
+		// If ESC, Quit the app
+		/*if(asKey == VK_ESCAPE && !(DevConsole::GetInstance()->IsOpen())) 
 		{
-			unsigned char asKey = (unsigned char) wParam;
-			InputSystem::GetInstance()->ProcessKeyUp(asKey);
-			return false;
-			break;
-		}
-		
-		//mouse info
-		case WM_ACTIVATE:
+		g_isQuitting = true;
+		return false;
+		}*/
+
+		break;
+	}
+
+	// Raw physical keyboard "key-was-just-released" event (case-insensitive, not translated)
+	case WM_KEYUP:
+	{
+		unsigned char asKey = (unsigned char) wParam;
+		InputSystem::GetInstance()->ProcessKeyUp(asKey);
+		break;
+	}
+
+	// handle mouse movement =========================================================================================
+	case WM_ACTIVATE:
+	{
+		bool is_active = (WA_INACTIVE != LOWORD( wParam ));
+		if (InputSystem::GetInstance()->GetMouse()->GetMouseMode() == MOUSE_RELATIVE_MODE)
 		{
-			bool is_active = (WA_INACTIVE != LOWORD( wParam ));
-			if(Window::GetInstance()->GetHandle() == HWND(lParam))
+			if (Window::GetInstance()->GetHandle() == HWND(lParam))
 			{
 				InputSystem::GetInstance()->GetMouse()->MouseLockToScreen(!is_active);
-			}		
+			}
 			else
 			{
 				InputSystem::GetInstance()->GetMouse()->MouseLockToScreen(is_active);
 			}
-		}
+		}			
+	}
 
-		case WM_LBUTTONDBLCLK:
-		{
-			InputSystem::GetInstance()->ProcessMouseInput(wParam);
-			break;
-		}
+	// handle left mouse button =========================================================================================
+	case WM_LBUTTONDOWN:
+	{
+		InputSystem::GetInstance()->ProcessMouseButtons(wParam);
+		break;
+	}
 
-		case WM_LBUTTONDOWN:
-		{
-			InputSystem::GetInstance()->ProcessMouseInput(wParam);
-			break;
-		}
+	case WM_LBUTTONUP:
+	{
+		InputSystem::GetInstance()->ProcessMouseButtons(wParam);
+		break;
+	}
 
-		case WM_LBUTTONUP:
-		{
-			InputSystem::GetInstance()->ProcessMouseInput(wParam);
-			break;
-		}
+	// handle right mouse button =========================================================================================
 
-		case WM_RBUTTONUP:
-		{
-			InputSystem::GetInstance()->ProcessMouseInput(wParam);
-			break;
-		}
+	case WM_RBUTTONDOWN:
+	{
+		InputSystem::GetInstance()->ProcessMouseButtons(wParam);
+		break;
+	}
 
-		case WM_RBUTTONDOWN:
-		{
-			InputSystem::GetInstance()->ProcessMouseInput(wParam);
-			break;
-		}
+	case WM_RBUTTONUP:
+	{
+		InputSystem::GetInstance()->ProcessMouseButtons(wParam);
+		break;
+	}
 
-		case WM_RBUTTONDBLCLK:
-		{
-			InputSystem::GetInstance()->ProcessMouseInput(wParam);
-			break;
-		}
+	// handle double clicks =========================================================================================
+	case WM_LBUTTONDBLCLK:
+	{
+		InputSystem::GetInstance()->GetMouse()->m_doubleClickLeft = true;
+		break;
+	}
+
+	case WM_RBUTTONDBLCLK:
+	{
+		InputSystem::GetInstance()->GetMouse()->m_doubleClickRight = true;
+		break;
+	}
 	}
 
 	return true; 
@@ -134,9 +140,8 @@ void Initialize()
 	g_theApp->Initialize();
 }
 
-//-----------------------------------------------------------------------------------------------
-// One "frame" of the game.  Generally: Input, Update, Render.  We call this 60+ times per second.
-//
+//  One "frame" of the game.  Generally: Input, Update, Render.  We call this 60+ times per second.=========================================================================================
+
 void RunFrame()
 {
 	g_theApp->RunFrame();
